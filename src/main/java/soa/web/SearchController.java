a8a65adf5f90b73b8cdf064193bafa0f421f15a4
anicacortes <anagonzalor@gmail.com>
2016-12-07 16:13:46 +0100

    Max limit set

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+
 
 @Controller
 public class SearchController {
@@ -23,6 +25,17 @@ public class SearchController {
     @RequestMapping(value="/search")
     @ResponseBody
     public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+        HashMap<String, Object> header = new HashMap<>();
+        if(q.contains("max")) {
+            int start = q.indexOf("max");
+            int numIndex = start + 4;
+            String num = q.substring(numIndex);
+            int number = Integer.parseInt(num); //number has the max limit
+            q = q.substring(0, start-1);        //q contains only the word asked
+            header.put("CamelTwitterCount", number);
+        }
+        header.put("CamelTwitterKeywords",q);
+        return producerTemplate.requestBodyAndHeaders("direct:search", "", header);
+
     }
 }
